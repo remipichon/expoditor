f8e677e671179f58279401a843586454a4cd8742
Kiki <Kiki@Kiki-folio>
2014-02-19 20:14:24 +0100

    elements dans la slide impossible d'avoir un render séparé

--- client/client.js
+++ client/client.js
@@ -1,280 +1,252 @@
-if (Meteor.isClient) {
-    var subscriptionSlide = Meteor.subscribe("slides");
-    Session.set("slideSubscription", "slides");
-    Meteor.subscribe("slidesLock");
-
-    Slides = new Meteor.Collection("slides");
-    SlidesLock = new Meteor.Collection("slidesLock");
-
-    Meteor.startup(function() {
-    });
-
-    Template.hello.greeeting = function() {
-        return "Click to create some slides";
-    };
-
-    //create a slide
-    Template.createPlaneSlide.events({
-        'click input': function() {
-            var d = new Date;
-            var title = d.getHours() + ":" + d.getMinutes() + ":" + d.getMilliseconds();
-            createSlide({title: title, type: 'planeSlide'});
-        }
-    });
-
-    Template.createJmpressSlide.events({
-        'click input': function() {
-            var d = new Date;
-            var title = d.getHours() + ":" + d.getMinutes() + ":" + d.getMilliseconds();
-            createSlide({title: title, type: 'jmpressSlide'});
-        }
-    });
-
-    //subscribe to plane or jmpress or all (exclu)
-    Template.loadAllSlide.events({
-        'click input': function() {
-            $("#slideArea").jmpress("deinit");
-            subscriptionSlide.stop();            
-//            $("#slideArea").empty();
-//function(){$("#slideArea").empty();}
-
-            subscriptionSlide = Meteor.subscribe("slides");
-            
-            Session.set("slideSubscription", "slides");
-        }
-    });
-    Template.loadPlaneSlide.events({
-        'click input': function() {
-            $("#slideArea").jmpress("deinit");
-            subscriptionSlide.stop();
-            
-            subscriptionSlide = Meteor.subscribe("planeSlides");
-            
-            Session.set("slideSubscription", "planeSlides");
-        }
-    });
-    Template.loadJmpressSlide.events({
-        'click input': function() {
-            subscriptionSlide.stop();
-//            initJmpress();
-
-            subscriptionSlide = Meteor.subscribe("jmpressSlides");
-            
-            Session.set("slideSubscription", "jmpressSlides");
-            //magouille parce que le callback de subscribe n'est pas un callback de Template.render (lui meme étant un callback)
-            setTimeout(initJmpress, 200);
-
-        }
-    });
-
-    //init slideArea with all slides presents in db
-    Template.slideArea.slides = function() {
-//        return Slides.find({}).fetch();
-        return Slides.find({});
-    };
-
-    //callback of render to add draggable and fadeInt when first render
-    Template.slide.rendered = function() {
-        //une bonne partie de cela devrait disparaitre lorsque le draggable de jquery sera enlevé        
-        //BAD BAD BAD BAD BAD il faut trouver un moyen pour que ce soit meteor qui s'en charge !
-        self = this;
-        var $slide = $(self.find(".slide"));
-        var $slide = $("#" + this.data._id);
-        var left = self.data.left;
-        var top = self.data.top;
-        var ratio = 10;
-        console.log("render :",this.data._id, top, left);
-
-        //pour toutes
-        if (self.data.toFadeIn) { //pour ne fadeIn qu'une fois
-            $("#" + self.data._id).fadeOut(0).fadeIn(1000);  //petite magouille
-            self.data.toFadeIn = false;
-        }
-        //title
-        $slide.children('.title').html(self.data.title);
-
-        //pos CSS (mode draggable) et jmpress       
-        $slide.attr("data-x", parseInt(left) * ratio).attr("data-y", parseInt(top) * ratio);
-
-        var type = Session.get("slideSubscription");
-        if (type === "planeSlides" || type === "slides") {
-            //pour plane
-//            console.log("render plane et all");
-            $slide.css("left", left).css("top", top);
-            $slide.draggable();
-        }
-
-        if (type === "jmpressSlides") {
-            //pour jmpress
-            console.log("render jmpress");
-            //pas top
-            $slide.css("left", 0).css("top", 0);
-
-            //lors de l'abonnement, on crée toutes les slides d'un coup puis jmpress passe par là.
-            //donc on ne reinit les slides qu'au rerender (je crois qu'il y a un astuce meteor pour m'eviter de faire ca)
-            if ($("#slideArea").jmpress('initialized')) {
-
-                //encore une magouille. Si #slideArea a plus d'un fils c'est que $slide en est un, il faut donc la deplacer dans le div container de camera jmpress
-                if ($("#slideArea >").length != 1) {
-                    $("#slideArea div:not(.slide)").append($slide);
-                }
-
-                console.log("init de la slide");
-                //deplacement de slide
-//            $("#slideArea >").append($slide);
-                $("#slideArea").jmpress('init', $slide);
-            }
-        }
-
-
-    };
-
-    //petite magouille in order to fadeInt when create only
-    Template.slide.created = function() {
-        this.data.toFadeIn = true;
-    };
-    
-    Template.slide.destroyed = function(){
-        console.log("slide destroyed", this.data._id);
-        //si un detruit une slide de jmpress, il faut l'enlever du DOM
-        if( Session.get("slideSubscription","jmpressSlides") ){ //je comprend pas pourquoi c'est fire à chaque fois, mais ca pose pas de probleme alors je verra plus tard
-            console.log("       slide jmpress removed");
-            $("#"+this.data._id).remove();
-        }
-    }; 
-
-    //update title
-    Template.slide.events({
-        'dblclick': function() {
-            //mise en place du lock si slide dispo
-            var userId = Meteor.userId();
-            var lock = SlidesLock.findOne({slideId: this._id});
-            if (typeof lock == 'undefined' || lock.userId == null) {
-                if (typeof lock == 'undefined') {
-                    SlidesLock.insert({slideId: this._id, userId: userId, type: 'title', slideTitle: this.title});
-                } else {
-                    SlidesLock.update(
-                            SlidesLock.findOne({slideId: this._id})._id,
-                            {$set: {
-                                    userId: userId, type: 'title', slideTitle: this.title //, slideId: this._id
-                                }});
-                }
-                console.log("update title, add lock to slide", this._id);
-                updateSlideTitle(this);
-            } else {
-                alert("lock set by " + lock.userId);
-            }
 
 
 
-        }
-    });
-
-    //update pos on move
-    Template.slide.events({
-        'mousedown, mousemove': function(event) {
-            updateSlidePosMove(this, event);
-        }
-    });
-
-    //update pos slide at the end of a move
-    Template.slide.events({
-        'mouseup': function(event) {
-            updateSlidePos(this, event);
-        }
-    });
-
-    //try : catch delete to add fadeOut (doen't work on others clients)
-    Template.deleteSlide.events({
-        'click': function(event) {
-            event.stopPropagation();
-            var self = this;
-            $("#" + this._id).fadeOut(1000, function() {
-                console.log("delete slide");
-                Slides.remove(self._id);
-            });
-        }
-    });
+/*
+le doublie click sur un elemen fire le double click sur la slide et avec du caca en plus 
+dans la mesire ou le promp est vide et que le title est pas vraiment mis à jour
+*/
 
 
-    //template method
-    Template.slide.getJmpressData = function(axis) { //pas encore utilisé à cause du draggable de jqueryreu
-        var ratio = 10;
-        switch (axis) {
-            case "x":
-                var coord = parseInt(this.left) * ratio;
-                break;
-            case "y":
-                var coord = parseInt(this.top) * ratio;
-                break;
-            case "z":
-                var coord = 0;
-                break;
-            default:
-                return "";
 
-        }
-        return 'data-' + axis + '="' + coord + '"';
-    };
+  
+
+
+var subscriptionSlide = Meteor.subscribe("slides");
+//var subscriptionslidesWithElements = Meteor.subscribe("slidesWithElements");
+
+Slides = new Meteor.Collection("slidesWithElements");
+
+Template.slide.elements = function(){
+    return this.elements;
+};
+
+
+Template.slideArea.slides = function() {
+    return Slides.find({});
+};
+
+Template.element.rendered = function(){
+    console.log("element.rendered",this.data._id);
 }
 
-
-initJmpress = function() {
-    console.log("init jmpress");
-    //preparer toutes les slides, ajout les attributs data-
-//    $('#slideArea .slide').each(function(index, slide) {
-//        var $slide = $(slide);
-//        $slide.attr("data-x", parseInt($slide.css("left")) * 10);
-//        $slide.attr("data-y", parseInt($slide.css("top")) * 10);
-//        $slide.attr("data-z", 0);
-////     $slide.removeAttr("style");
-//        $slide.children('.title').removeClass("title").addClass("H1Text");
-//    });
+Template.slide.rendered = function() {
+    console.log("slide.rendered",this.data._id);
+    self = this;
+    var $slide = $(self.find(".slide"));
+    var $slide = $("#" + this.data._id);
+    $slide.draggable();
+    return;
 
 
-    //lancer jmpress
-    $('#slideArea').jmpress({
-        viewPort: {
-            height: 400,
-            width: 3200,
-            maxScale: 1
-        }
-    });
+//    var left = self.data.left;
+//    var top = self.data.top;
+//    var ratio = 10;
+//    console.log("render slide:", this.data._id, top, left);
+//
+//
+//    //title
+//    $slide.children('.title').html(self.data.title);
+//
+//    //pos
+//    $slide.css("left", left).css("top", top);
+
+
 };
+
+
+Template.addElement.events({
+    'click': function(event) {
+        event.stopPropagation();
+        var $element = $(event.target);
+        createSlideElement($element.parent(".slide").attr("id"));
+    }
+});
+
+
+//update element
+Template.element.events({
+    'click': function(event) {
+        event.stopPropagation();
+        var $element = $(event.target);
+        updateSlideElement($element.parent().parent(".slide").attr("id"), this);
+    }
+});
+
+Template.deleteElement.events({
+    'click': function(event) {
+        event.stopPropagation();
+        var $element = $(event.target);
+        deleteSlideElement($element.parent().parent(".slide").attr("id"), this);
+    }
+});
+
+getIndexOfElement = function(slideId, elementId) {
+    var allElements = Slides.findOne({_id: slideId}).elements;
+    for (var i = 0; i < allElements.length; i++) {
+        if (allElements[i]._id === elementId)
+            return i;
+    }
+    return -1;
+};
+
+
+updateSlideElement = function(slideId, element) {
+    console.log("update element ", element._id, " of slide ", slideId);
+    var content = prompt("new title", element.content);
+
+    if (content != null) {
+        //cancel, pas d'update
+        //obligé de passer par l'index afin d'éviter //Not permitted. Untrusted code may only update documents by ID. [403]
+        var elementIndex = getIndexOfElement(slideId, element._id); //pas aussi simple, il faut extraire slide.id et le chercher dans les elements de .slides
+
+        var query = {};                 // create an empty object
+        query['elements.' + elementIndex + '.content'] = content;  // and then populate the variable key
+
+        Slides.update(
+                {_id: slideId},
+        {
+            $set: query  //attention, c'est un $set donc ca écrase tout ce qui est maché 
+        }
+        );
+    }
+};
+
+deleteSlideElement = function(slideId, element) {
+    console.log("delete element ", element._id, " of slide ", slideId);
+
+    Slides.update(
+            {_id: slideId},
+    {
+        $pull: {elements:
+                    {_id: element._id}
+        }
+    }
+    );
+
+    return;
+
+};
+
+/* Exception from Deps afterFlush function: Error: Can't create second landmark in same brancj*/
+/*https://github.com/meteor/meteor/issues/281#issuecomment-16191927*/
+/*https://github.com/meteor/meteor/issues/281#issuecomment-28704924*/
+Handlebars.registerHelper('labelBranch', function(label, options) {
+    var data = this;
+    return Spark.labelBranch(Spark.UNIQUE_LABEL, function() {
+        return options.fn(data);
+    });
+});
+
+
+
+createSlideElement = function(slideId) {
+    var d = new Date;
+    var title = "ele:" + d.getHours() + ":" + d.getMinutes() + ":" + d.getMilliseconds();
+    console.log("create element on slide ", slideId);
+
+    return Slides.update(
+            {_id: slideId},
+    {$push: {
+            elements: {
+                _id: Random.id(),
+                type: "text",
+                content: title
+            }
+        }
+    }
+    );
+//        db.slidesWithElements.update(
+//            {_id: "PnNRaQyCiz2J5Fv5D"},
+//    {$push: {
+//            elements: {
+//                _id: "12341234",
+//                type: "text",
+//                content: "title"
+//            }
+//        }
+//    }
+//    );
+
+};
+
+
+//TODO
+//delete el
+
+
+
+
+
+
+
+
+
+
+
+//update title
+Template.slide.events({
+    'dblclick': function(event) {
+        event.stopPropagation();
+        //mise en place du lock si slide dispo
+        console.log("update title slide canceled by events");
+        return;
+        updateSlideTitle(this);
+    }
+  
+});
+
+
+
+//update pos slide at the end of a move
+Template.slide.events({
+    'mouseup': function(event) {
+//        event.stopPropagation();//jquery doit pouvoir recevoir l'event pour gerer comme il faut le draggable
+        updateSlidePos(this, event);
+    }
+});
+
+
+Template.deleteSlide.events({
+    'click': function(event) {
+        event.stopPropagation();
+        event.stopPropagation();
+        var self = this;
+        Slides.remove(self._id);
+    }
+});
+
+
+
+
 
 
 
 createSlide = function(options) {
+    var d = new Date;
+    var title = d.getHours() + ":" + d.getMinutes() + ":" + d.getMilliseconds();
     var top = 50;
     var left = 50;
     return  Slides.insert({
-        title: options.title, 
-        top: top, 
-        left: left, 
-        type: options.type, 
-        sub1: [   //mes slides
-            
-            {    //une slide
+        title: title,
+        top: top,
+        left: left,
+        type: "options.type",
+        elements: [
+            {
                 _id: "id1",
-                attr1: "one"
+                type: "text",
+                content: "text1"
+                        //mettre tous les attributs atomique à ce niveau
+                        //pos est atomique car pos.x et pos.y sont màj en meme temps par un seul client
+                        //content pour text est atomique, font l'est aussi si on peut permettre qu'un client modify the font while another edit text
             },
             {
-                _id: "id2",
-                attr1: "two"
-        
-        }
+                _id: "id1",
+                type: "text",
+                content: "text2"
+
+            }
         ]
-//        sub1: {   //mes slides
-//            attr11: "11Value",
-//            sub12: {    //une slide
-//                attr121: "121Value",
-//                attr122: "122Value"
-//            },
-//            sub13: {
-//                attr131: "131Value",
-//                attr132: "132Value"
-//            }
-//        }
     });
 };
 
@@ -290,18 +262,15 @@ updateSlideTitle = function(slide) {
         });
     }
 
-    //supression du lock
-    SlidesLock.update(
-            SlidesLock.findOne({slideId: slide._id})._id,
-            {$set:
-                        {userId: null}
-            });
+
 };
 
 updateSlidePos = function(slide, event) {
     var $slide = $("#" + slide._id);
-    var top = $slide.css('top');
-    var left = $slide.css('left');
+    var top = parseInt($slide.css('top'));
+    var left = parseInt($slide.css('left'));
+
+    console.log("updateSlidePos :", left, top);
 
     return Slides.update(slide._id, {$set:
                 {top: top, left: left}
@@ -309,35 +278,22 @@ updateSlidePos = function(slide, event) {
 };
 
 
-updateSlidePosMove = function(slide, event) {
-    var $slide = $("#" + slide._id);
-    var newTop = parseInt($slide.css('top'));
-    var newLeft = parseInt($slide.css('left'));
 
-    //histoire de pas trop surcharger le server, on update pas tout le temps
-    var slideDb = Slides.findOne({_id: slide._id});
-    var oldTop = parseInt(slideDb.top);
-    var oldLeft = parseInt(slideDb.left);
-    //console.log("newTop",newTop,"oldTop",oldTop);
 
-    var distance = Math.sqrt(
-            Math.pow(newTop - oldTop, 2) + Math.pow(newLeft - oldLeft, 2)
-            );
 
-    //trop courte
-    if (distance < 5) {
-        //console.log("skip update",distance);
-        return;
-    }
-
-    Slides.update(slide._id, {$set:
-                {top: newTop, left: newLeft}
+/** pour du debug sur le render **/
+function logRenders () {
+    _.each(Template, function (template, name) {
+      var oldRender = template.rendered;
+      var counter = 0;
+ 
+      template.rendered = function () {
+        console.log(name, "render count: ", ++counter);
+        oldRender && oldRender.apply(this, arguments);
+      };
     });
-};
-
-
-
-
-
+  }
+  
+//  logRenders();
 
 
--- server/server.js
+++ server/server.js
@@ -1,43 +1,69 @@
-addHandleSlide = function(self) {
+//
+//publishSlidesWithElements = function(){
+//    return Slides.find({});
+//};
 
+publishSlides = function() {
+    var self = this;
+
+    //il faut stop le handle oiu fiare d'autres trucs je sais plus quoi
+    var handleSlides = Slides.find({}).observeChanges({
+        added: function(id, slide) {
+            console.log("added", id);
+            self.added("slidesWithElements", id, slide);
+            
+            //gestion de l'array elements de chaque slide
+            var handleElementsBySlides = Slides.find({_id: id},{fields:{elements:1}}).observeChanges({
+               changed: function(id,fields){
+                   console.log("elements changed for slide",id);
+                   self.changed("slidesWithElements", id, fields);
+               }
+            }); 
+        },
+        changed: function(id, fields) {            
+            if( typeof fields.elements !== "undefined"){
+//                console.log("elements changed (add/update/remove)");
+                return;
+            }
+            console.log("changed", id,fields);
+            self.changed("slidesWithElements", id, fields);
+        },
+        removed: function(id, slide) {
+            console.log("removed", id);
+            self.removed("slidesWithElements", id, slide);
+        }
+    });
 };
 
-publishSlides = function() { //pseudo slideshow
-//    return Slides.find({});
-    var pseudoSlideshowId = "CxqBw4c2oyDDoHkYr";
+publishSlides2 = function() {
+
+    return Slides.find({});
+
+    pseudoSlideshowId = "!!!!!!!!!!!!!!";
+
     var self = this;
-//    console.log("\nobservechange\n",
-//            Slides.find({_id: "qkBPHySDuarpWvmPs"
-//                , "sub1.attr121": "121Value"
-//    }
-//    , {fields: {"sub1.$": 1}}  //donc là on ne chope qu'une slide
-//    )
-//            .fetch()
-//
-//            );
-    
-    //publication du slideshow
-     var slide = Slides.findOne({
-                _id: pseudoSlideshowId
-            });
-     console.log("publication pseudo slideshow", slide._id);
-     self.added("slides", pseudoSlideshowId, slide);
+
+    var slide = Slides.findOne({
+        _id: pseudoSlideshowId
+    });
+
+    self.added("slides", pseudoSlideshowId, slide);
 
 
     //pour ajouter une slide, on ne regarde que l'attribut slide (sub1) (ne prend pas en compte la structure complexe)
-    var handle = Slides.find({_id: pseudoSlideshowId, "sub1.newOne":1}).observeChanges({//il faut restreindre l'observe
-        changed: function(id,fields) {  
+    var handle = Slides.find({_id: pseudoSlideshowId, "sub1.newOne": 1}).observeChanges({//il faut restreindre l'observe
+        changed: function(id, fields) {
             //collection, id, fields
             var slide = Slides.find({
                 _id: id
             });
-            console.log("add pseudo slide", id,fields);
+            console.log("add pseudo slide", id, fields);
 //          
-            Slides.update({_id: pseudoSlideshowId, "sub1.newOne":1},{$set:{ "sub1.$.newOne":0}});
-              self.added("slides", id, slide);
+            Slides.update({_id: pseudoSlideshowId, "sub1.newOne": 1}, {$set: {"sub1.$.newOne": 0}});
+            self.added("slides", id, slide);
         }
     });
-    
+
     return;
 
     //pour les modifications sur les slides
@@ -77,11 +103,11 @@ publishSlides = function() { //pseudo slideshow
             sub1: {
                 _id: "id3",
                 attr1: "three",
-                newOne : 1 //magouille degeulasse, flag pour dire au publish de l'ajouter
-        
+                newOne: 1 //magouille degeulasse, flag pour dire au publish de l'ajouter
+
             }
         }
-    }); 
+    });
 
 
     //modification d'une pseudo slide (
@@ -93,156 +119,16 @@ publishSlides = function() { //pseudo slideshow
 
 };
 
-Meteor.methods({
-    //ne sert à rien
-    testUpdateSubDocument: function(id, str) {
-//        console.log("meteor methods ",id,str);
-        Slides.update({_id: id, "subDocument": "subSubDocument"},
-        {$set:
-                    {subSubAttr1: str}
-        }
-        );
-    }
-});
+
+Meteor.publish("slides", publishSlides);
+//Meteor.publish("slidesWithElements", publishSlidesWithElements);
+
+Slides = new Meteor.Collection("slidesWithElements");
 
 
-publishJmpressSlides = function() {
-    return Slides.find({type: "jmpressSlide"});
-};
-
-publishPlaneSlides = function() {
-    return Slides.find({type: "planeSlide"});
-};
-
-publishSlidesLock = function() {
-    return SlidesLock.find({}); //plus tard, ne pas envoyer l'id mais l'email, ou null si null
-};
-
-if (Meteor.isServer) {
-
-    Slides = new Meteor.Collection("slides");
-    SlidesLock = new Meteor.Collection("slidesLock");
-
-    //publication des données
-    Meteor.publish('slides', publishSlides);
-    Meteor.publish('planeSlides', publishPlaneSlides);
-    Meteor.publish('jmpressSlides', publishJmpressSlides);
-    Meteor.publish('slidesLock', publishSlidesLock);
 
 
-    Meteor.startup(function() {
-        // code to run on server at startup
-        if (Slides.find().count() != 0) {
-            Slides.remove();
-        }
-    });
-
-    SlidesLock.allow({
-        insert: function(userId, lock, fields, modifier) {
-            return true;
-        },
-        update: function(userId, newLock, fields, modifier) {
-            console.log("debug : slidesLock.update ", modifier);
-
-            //verifie si le lock existe bel et bien
-            var lock = SlidesLock.findOne({_id: newLock._id});
-            if (typeof lock == 'undefined') {
-                console.log("info : slideslock.update : lock doesn't exist ", newLock._id);
-                return false;
-            }
-
-            //si ajout d'un lock
-            if (modifier.$set.userId != null) {
-                //verification que le lock n'écrase pas un lock
-                if (lock.userId != null) {
-                    console.log("info : slidesLock.update : a lock is already set : db : ", lock._id, "new : ", newLock._id);
-                    return false;
-                }
-                console.log("info : slidesLock.update : add lock OK");
-                return true;
-            }
-
-            //si suppresion d'un lock
-            if (userId != lock.userId) {
-                //verification que le lock est bien celui du user
-                console.log("info : slidesLock.update : client try to remove a lock not set by him : robber ", userId, " locker : ", lock.userId);
-                return false;
-            } else {
-                console.log("info : slidesLock.update : remove lock OK");
-                return true;
-            }
-
-            console.log("info : slidesLock.update : case not expected, return false");
-            return false;
-        },
-        remove: function() {
-            //uniquement coté server si besoin
-            return false;
-        }
-    });
 
 
-    //le server ontrole certaines actions sur les slides
-    Slides.allow({
-        insert: function() {
-            console.log("info : slides.allow.insert : true");
-            return true;
-        },
-        update: function(userId, slide, fields, modifier) {
-            return true;
-            console.log("info : slides.allow.update");
-            //update de title et position en exclusivité avec priorité au title
-
-            //texte
-            if (_.contains(fields, 'title')) {
-                var lock = SlidesLock.findOne({$and: [
-                        {slideId: slide._id}, {userId: userId}
-                    ]});
-
-                if (typeof lock == 'undefined') {
-                    console.log("info : slides.allow.update : client trying to update without lock slide : ", slide._id, "client :", userId);
-                    return false;
-                }
-                console.log("info : slides.allow.update : allow title update");
-                return true;
-            }
-
-
-            //position
-            if (_.contains(fields, 'top') || _.contains(fields, 'left')) {
-                return true;
-
-                var topSlide = parseInt(modifier.$set.top);
-                var leftSlide = parseInt(modifier.$set.left);
-                var widthSlide = 150;
-                var heightSlide = 30;
-                var widthForbidenZone = 200;  // 2 d !
-                var heightForbidenZone = 200;
-                var topForbidenZone = 200;
-                var leftForbidenZone = 200;
-
-                //si la mise à jour de la position proposée est dans la zone, on refuse l'update
-                if (
-                        (topForbidenZone < topSlide && topSlide < topForbidenZone + heightForbidenZone ||
-                                topForbidenZone < topSlide + heightSlide && topSlide + heightSlide < topForbidenZone + heightForbidenZone)
-                        &&
-                        (leftForbidenZone < leftSlide && leftSlide < leftForbidenZone + widthForbidenZone ||
-                                leftForbidenZone < leftSlide + widthSlide && leftSlide + widthSlide < leftForbidenZone + widthForbidenZone)
-                        ) {
-                    console.log("info : slides.allow.update pos : refuse pos update, forbidden zone");
-                    return false;
-                }
-                console.log("info : slides.allow.update pos : allow pos update");
-                return true;
-            }
-
-            console.log("info : slidesLock.allow.update pos : case not expected, return false");
-            return false;
-        },
-        remove: function() {
-            return true;
-        }
-    });
-}
 
 
