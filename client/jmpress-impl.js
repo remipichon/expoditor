67876aa97841661bcedab33b93d2e0c24b3389e0
remipichon <pichon.remi.pr@gmail.com>
2014-05-10 14:18:10 +0200

    add current edit slide title next to slideshow title

--- client/jmpress-impl.js
+++ client/jmpress-impl.js
@@ -12,6 +12,18 @@ Template.jmpressContainer.slides = function() {
 };
 
 
+/**
+ * jmpress mode
+ */
+Template.elementsAreaJmpress.elements = function() {
+    return Elements.find({
+        slideReference: {
+            $in: [this._id]
+        }
+    });
+};
+
+
 cloneJmpressSlide = function(sl) {
     var slide = $(sl);
     var clone = slide.clone();
@@ -64,6 +76,64 @@ Template.jmpressSlide.getJmpressData = function(axis) {
     return coord;
 };
 
+
+/**
+ * calculate position and size of an element in slide content editor mode according to ratioSlideshowMode
+ * @param  {string} axis which CSS style is needed
+ * @return {int}      value of the CSS style 
+ */
+Template.elementJmpress.getEditorData = function(axis) { //pas encore utilisé à cause du draggable de jqueryreu
+
+    if (typeof this.CSS === 'undefined') { //works here because elementCurrendEditing are #constant
+        console.log("elementJmpress.getEditorData", this._id);
+
+        //a a factoriser avec l'observeChanges
+        this.center = {
+            x: parseFloat(this.displayOptions.editor.positions.x),
+            y: parseFloat(this.displayOptions.editor.positions.y)
+        };
+        this.size = {
+            width: parseFloat(this.displayOptions.editor.size.width),
+            height: parseFloat(this.displayOptions.editor.size.height)
+        }
+        this.ratio = {
+            top: ratioContentMode,
+            left: ratioContentMode
+        }
+        delete this.CSS;
+        posToCSS.call(this);
+    }
+
+
+    switch (axis) {
+        case "x":
+            var coord = this.CSS.left; 
+            break;
+        case "y":
+            var coord = this.CSS.top; 
+            break;
+        case "z":
+            var coord = 0;
+            break;
+        case "scaleX":
+            var coord = 1; 
+            break;
+        case "scaleY":
+            var coord = 1; 
+            break;
+        case "h":
+            var coord = this.displayOptions.editor.size.height / 1;
+            break;
+        case "w":
+            var coord = this.displayOptions.editor.size.width / 1;
+            break;
+        default:
+            return "";
+    }
+
+
+    return coord;
+};
 
 
 /**** render ****/
--- client/view.js
+++ client/view.js
@@ -1,7 +1,15 @@
 
-Template.slideshowTitle.title= function(){
+Template.slideshowTitle.slideshowTitle= function(){
 	if(typeof Slideshow.findOne({}) === 'undefined'){
 		return 'no slideshow loaded';
 	}
 	return Slideshow.findOne({}).informations.title;
+}
+
+
+Template.slideshowTitle.slideTitle= function(){
+	if(typeof CurrentEditing.findOne({}) === 'undefined'){
+		return null;
+	}
+	return '>> '+CurrentEditing.findOne({}).informations.title;
 }
